fafdcaafa84b536e6e0c548cf1ac2a24aa3e9a32
ByungSoo Ye <ybs680318@gmail.com>
2021-05-26 13:25:55 +0900

    Add - SamsungKeyboard 클래스를 추가 - KeyboardManager 클래스의 get_keyboard_input 메소드를 수정 ** 기존의 클래스를 수정했으므로 개방 폐쇄 원칙에 위배됨

--- example02.py
+++ example02.py
@@ -25,5 +25,29 @@ class KeyboardManager:
 
     def get_keyboard_input(self):
         """유저가 키보드로 입력한 내용을 받아오는 메소드"""
-        return self.keyboard.send_keyboard_input()
+        if isinstance(self.keyboard, AppleKeyboard):
+            return self.keyboard.send_keyboard_input()
+        elif isinstance(self.keyboard, SamsungKeyboard):
+            return self.keyboard.give_user_input()
 
+
+class SamsungKeyboard:
+    """삼성 키보드 클래스"""
+    def __init__(self):
+        """키보드 인풋"""
+        self.user_input = ""
+
+    def save_user_input(self, input):
+        """키보드 인풋 저장 메소드"""
+        self.user_input = input
+
+    def give_user_input(self):
+        """키보드 인풋 전송 메소드"""
+        return self.user_input
+keyboard_manager = KeyboardManager()
+
+saumsung_keyboard = SamsungKeyboard()
+
+keyboard_manager.connect_to_keyboard(saumsung_keyboard)
+saumsung_keyboard.save_user_input("안녕하세요 삼성 키보드입니다.")
+print(keyboard_manager.get_keyboard_input())
