b79ab374d1511d82f27b111be77c893b226b9c35
ByungSoo Ye <69490984+DevelopSoo@users.noreply.github.com>
2021-05-28 01:15:05 +0900

    Modify - example03_b.py 수정 - Square 클래스는 Rectangle 클래스의 행동 규약을 지키기 어려움 - 상속을 받지 않는 것으로 수정

--- example03_b.py
+++ example03_b.py
@@ -31,42 +31,34 @@ class Rectangle:
         self._height = value if value > 0 else 1
 
 
-class Square(Rectangle):
+class Square:
     def __init__(self, side):
-        super().__init__(side, side)
+        self.side = side
+
+    def area(self):
+        """정사각형 넓이 계산 메소드"""
+        return self.side * self.side
 
     @property
-    def width(self):
-        """가로 변수 getter 메소드"""
-        return self._width
+    def side(self):
+        """한 변 getter 메소드"""
+        return self._side
 
-    @width.setter
-    def width(self, value):
-        """가로 변수 setter 메소드"""
-        self._width = value if value > 0 else 1
-        self._height = value if value > 0 else 1
+    @side.setter
+    def side(self, value):
+        """한 변 setter 메소드"""
+        self._side = value if value > 0 else 1
 
-    @property
-    def height(self):
-        """세로 변수 getter 메소드"""
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        """세로 변수 setter 메소드"""
-        self._width = value if value > 0 else 1
-        self._height = value if value > 0 else 1
 
 rectangle_1 = Rectangle(4, 6)
 
-rectangle_2 = Square(2)
+square = Square(2)
 
 rectangle_1.width = 3
 rectangle_1.height = 7
 
 print(rectangle_1.area()) # 21
 
-rectangle_2.width = 3
-rectangle_2.height = 7
+square.side = 10
 
-print(rectangle_2.area()) # 49
+print(square.area()) # 100
